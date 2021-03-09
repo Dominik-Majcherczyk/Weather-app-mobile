e2ddb87641a4366cc92f15256eee5e981be62a03
Dominik-Majcherczyk <64542753+Dominik-Majcherczyk@users.noreply.github.com>
2021-03-09 22:36:33 +0100

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { TextInput } from "react-native-gesture-handler";
+import Weather from "./components/Weather";
+import LocalizationButton from "./components/LocalizationButton";
+import styles from "./styles/app.js";
 
+const STORAGE_KEY = "@weatherApp:key";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{todo !== "" ? `${todo}` : "there is no todo"}</Text>
       <StatusBar style="auto" />
+
+      <TextInput
+        style={styles.input}
+        onChange={(e) => setTodo(e.target.value)}
+        value={todo}
+      />
     </View>
   );
 }
@@ -14,8 +25,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  input: {
+    backgroundColor: "#333333",
+    color: "#fff",
   },
 });
