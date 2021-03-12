b8fa20e23c396d9d7adcaf8221886493bfd68073
Dominik-Majcherczyk <64542753+Dominik-Majcherczyk@users.noreply.github.com>
2021-03-12 22:06:31 +0100

    rebuild

--- App.js
+++ App.js
@@ -2,35 +2,37 @@ import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { TextInput } from "react-native-gesture-handler";
-import Weather from "./components/Weather";
-import LocalizationButton from "./components/LocalizationButton";
-import styles from "./styles/app.js";
 
-const STORAGE_KEY = "@weatherApp:key";
 export default function App() {
+  const [value, onChangeText] = useState("Useless Placeholder");
+  const [city, setCity] = useState("");
+  function getCity(value) {
+    fetch(
+      "http://dataservice.accuweather.com/locations/v1/cities/search?apikey=lxxucFd3EEaDSpxcFbTVyROFKL3tWxsG&q=Brzeszcze"
+    ).then(function (response) {
+      if (response.status !== 200) {
+        console.log(
+          "Looks like there was a problem. Status Code: " + response.status
+        );
+        return;
+      }
+      // Examine the text in the response
+      response.json().then(function (data) {
+        console.log(data[0].LocalizedName);
+        setCity(data[0].LocalizedName);
+      });
+    });
+  }
   return (
-    <View style={styles.container}>
-      <Text>{todo !== "" ? `${todo}` : "there is no todo"}</Text>
-      <StatusBar style="auto" />
-
+    <View>
+      <Text>wprowadz miaaasto</Text>
       <TextInput
-        style={styles.input}
-        onChange={(e) => setTodo(e.target.value)}
-        value={todo}
+        style={{ height: 40, borderColor: "gray", borderWidth: 1 }}
+        onChangeText={(text) => onChangeText(text)}
+        value={value}
+        onSubmitEditing={() => getCity(value)}
       />
+      <Text>{city}</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  input: {
-    backgroundColor: "#333333",
-    color: "#fff",
-  },
-});
