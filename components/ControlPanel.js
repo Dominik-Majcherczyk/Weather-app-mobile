f6711be4a3b4d6404aed32df076346b77961a467
Dominik-Majcherczyk <64542753+Dominik-Majcherczyk@users.noreply.github.com>
2021-03-18 13:28:14 +0100

    added toast error message to city name search input

--- components/ControlPanel.js
+++ components/ControlPanel.js
@@ -1,14 +1,13 @@
 import React, { useState } from "react";
 import { View, StyleSheet, Text } from "react-native";
 import { TextInput } from "react-native-gesture-handler";
-import { useSelector, useDispatch } from "react-redux";
+import { useDispatch } from "react-redux";
 import { findCity } from "../reducers/city";
 
 const ControlPanel = () => {
   const [input, setInput] = useState();
-  const cityData = useSelector((state) => state.cityData);
+
   const dispatch = useDispatch();
-  console.log(cityData);
   return (
     <View style={styles.container}>
       <TextInput
@@ -17,8 +16,6 @@ const ControlPanel = () => {
         onChangeText={(text) => setInput(text)}
         onEndEditing={() => dispatch(findCity(input))}
       ></TextInput>
-      <Text>{cityData.cityName}</Text>
-      <Text>{cityData.cityId}</Text>
     </View>
   );
 };
--- components/WeatherDashboard.js
+++ components/WeatherDashboard.js
@@ -1,13 +1,34 @@
 import React from "react";
-import { ImageBackground, Text, View, StyleSheet } from "react-native";
+import {
+  ImageBackground,
+  Text,
+  View,
+  StyleSheet,
+  ToastAndroid,
+} from "react-native";
+import { useSelector } from "react-redux";
+
 const WeatherDashboard = () => {
   const image = {
     uri: "https://ak.picdn.net/shutterstock/videos/26398718/thumb/1.jpg",
   };
+  const cityData = useSelector((state) => state.cityData);
+
+  const showToast = () => {
+    ToastAndroid.show(
+      "The city was not found. Try again",
+      ToastAndroid.LONG,
+      ToastAndroid.TOP
+    );
+  };
+
+  console.log(cityData);
+  cityData.error ? showToast() : "";
   return (
     <View style={styles.container}>
       <ImageBackground source={image} style={styles.image}>
-        <Text></Text>
+        <Text style={styles.text}>{cityData.cityName}</Text>
+        <Text style={styles.text}>{cityData.cityId}</Text>
       </ImageBackground>
     </View>
   );
@@ -25,5 +46,8 @@ const styles = StyleSheet.create({
     flex: 1,
     resizeMode: "cover",
     justifyContent: "center",
+  },
+  text: {
+    color: "#fff",
   },
 });
